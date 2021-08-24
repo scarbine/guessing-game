7197a13fbd91daa404660ac7ca08f681179763eb
Sam Carbine <scarbine@me.com>
2021-08-24 15:08:50 -0500

    phase-7 complete

--- Program.cs
+++ Program.cs
@@ -12,10 +12,36 @@ namespace guessing_game
             int secrectNumber = rnd.Next(1,100);
             int userGuess = 0;
             int counter = 0;
-            int totalRounds = 4;
+            int totalRounds = 0;
             int currentRound = 1;
-            int roundsLeft = totalRounds;
+            int roundsLeft = 0;
             bool isSame = false;
+
+            Console.WriteLine("Welcome to the Guessing Game!");
+            Console.WriteLine("Please choose your level of difficulty. Enter the number of the level you would like to choose.");
+            Console.WriteLine("1) Easy");
+            Console.WriteLine("2) Medium");
+            Console.WriteLine("3) Hard");
+            string userDifficulty = Console.ReadLine();
+            int difficultyLevel = int.Parse(userDifficulty);
+            Console.WriteLine(difficultyLevel);
+            Console.WriteLine(userDifficulty);
+
+            if(userDifficulty == "3")
+            {
+                totalRounds = 4;
+                roundsLeft = 4;
+            }
+            else if( userDifficulty == "2")
+            {
+                totalRounds = 6;
+                roundsLeft = 6;
+            }
+            else
+            {
+                totalRounds = 8;
+                roundsLeft = 8;
+            }
 
             while(counter < totalRounds){
                 Game();
@@ -39,6 +65,8 @@ namespace guessing_game
 
             void Game()
             {
+
+            
             Console.WriteLine($"Round {currentRound}");
             // Console.WriteLine($"Secrect Number {secrectNumber}");
             Console.WriteLine($"Can you guess the secrect number? You have {roundsLeft} rounds left");
