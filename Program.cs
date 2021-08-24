da7c1ee7db787839f63d210b382612c33877a319
Sam Carbine <scarbine@me.com>
2021-08-24 15:28:25 -0500

    phase-8 complete. could add a counter to CheaterMode tho.

--- Program.cs
+++ Program.cs
@@ -16,35 +16,56 @@ namespace guessing_game
             int currentRound = 1;
             int roundsLeft = 0;
             bool isSame = false;
+            bool isCheater = false;
 
             Console.WriteLine("Welcome to the Guessing Game!");
             Console.WriteLine("Please choose your level of difficulty. Enter the number of the level you would like to choose.");
             Console.WriteLine("1) Easy");
             Console.WriteLine("2) Medium");
             Console.WriteLine("3) Hard");
+            Console.WriteLine("4) Cheater");
             string userDifficulty = Console.ReadLine();
             int difficultyLevel = int.Parse(userDifficulty);
             Console.WriteLine(difficultyLevel);
             Console.WriteLine(userDifficulty);
 
-            if(userDifficulty == "3")
+            if (userDifficulty == "1")
             {
-                totalRounds = 4;
-                roundsLeft = 4;
+                totalRounds = 8;
+                roundsLeft = 8;
             }
             else if( userDifficulty == "2")
             {
                 totalRounds = 6;
                 roundsLeft = 6;
             }
-            else
+            else if(userDifficulty == "3")
             {
-                totalRounds = 8;
-                roundsLeft = 8;
+                totalRounds = 4;
+                roundsLeft = 4;
+            }
+            else if( userDifficulty == "4")
+            {
+                totalRounds = 10;
+                roundsLeft =10;
+               isCheater = true;
             }
 
-            while(counter < totalRounds){
-                Game();
+            while(counter < totalRounds || isCheater){
+               if(isCheater)
+               {
+                   Game();
+                   if(userGuess == secrectNumber)
+                {
+                    Console.WriteLine($"You guessed it!");
+                   
+                    break;
+                }
+
+               } 
+               else
+               {
+                   Game();
                 counter++;
                 currentRound++;
                 roundsLeft--;
@@ -54,6 +75,7 @@ namespace guessing_game
                    
                     break;
                 }
+               }
             }
 
 
@@ -67,9 +89,9 @@ namespace guessing_game
             {
 
             
-            Console.WriteLine($"Round {currentRound}");
+            Console.WriteLine( isCheater ? "CheatMode!" : $"Round {currentRound}" );
             // Console.WriteLine($"Secrect Number {secrectNumber}");
-            Console.WriteLine($"Can you guess the secrect number? You have {roundsLeft} rounds left");
+            Console.WriteLine(isCheater ? "Can you guess the secrect number?" : $"Can you guess the secrect number? You have {roundsLeft} rounds left");
             string userGuessString = Console.ReadLine();
             int userGuessInt = int.Parse(userGuessString);
             userGuess = userGuessInt;
