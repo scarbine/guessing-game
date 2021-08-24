3f9378ff4b0bfb2d43d0a8487bca9f108e0fa78c
Sam Carbine <scarbine@me.com>
2021-08-24 13:26:01 -0500

    first-commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace guessing_game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Can you guess the secrect number?");
+            string userGuessString = Console.ReadLine();
+            int userGuessInt = int.Parse(userGuessString);
+            Console.WriteLine($" You guessed # {userGuessInt}");
+
+        }
+    }
+}
